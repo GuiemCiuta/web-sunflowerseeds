d35a1485f81f19aee1e26dc68f06468efbbfc9f6
Guillem Bagur <guillembagurmoll@gmail.com>
2024-06-09 22:29:38 +0200

    some changes

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("nav").innerHTML = `<ul class="nav__links">
+        <li class="nav__li">
+          <a href="./index.html" class="nav__link" data-role="nav.home"
+            >Inicio</a
+          >
+        </li>
+        <li class="nav__li">
+          <a
+            href="./sobre-el-proyecto.html"
+            class="nav__link"
+            data-role="nav.about"
+            >Sobre el proyecto</a
+          >
+        </li>
+        <li class="nav__li">
+          <a href="./contacto.html" class="nav__link" data-role="nav.contact"
+            >Contacto</a
+          >
+        </li>
+      </ul>`;
+});
+
 document.addEventListener("scroll", function () {
   const nav = document.getElementById("nav");
-  console.log("hola nav");
 
   if (window.scrollY <= 50) {
     nav.classList.remove("nav--add-contrast");
